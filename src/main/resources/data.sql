fff77f9df5f49e5cc8f7f8ea7318ca12202f18e0
Guilherme de Almeida Oliveira <guilhermeovergame@gmail.com>
2025-06-16 16:36:04 -0300

    Docker Compose atualizado

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
+-- select 1;
+
 
 truncate table cargo restart identity cascade;
 
 truncate table categoria restart identity cascade;
 
---truncate table cliente restart identity cascade;
+truncate table cliente restart identity cascade;
 
 truncate table endereco restart identity cascade;
 
@@ -15,7 +17,7 @@ truncate table feedback restart identity cascade;
 
 truncate table fornecedor restart identity cascade;
 
---truncate table funcionario restart identity cascade;
+truncate table funcionario restart identity cascade;
 
 truncate table item_pedido restart identity cascade;
 
@@ -123,69 +125,57 @@ VALUES('estoqueLote', 10, '10-10-2024', '30-02-2025', '10-2024-0038', 1, 1, 1),
 -- Clientes
 
 WITH novo_usuario AS (
-    INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
-        VALUES ('11111111111', '2014-11-11', 'usuario01@gmail.com', 'usuario01 da silva', '1dasilva', '1111-1111', 1)
-        RETURNING id
-)
+INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
+VALUES ('11111111111', '2014-11-11', 'usuario01@gmail.com', 'usuario01 da silva', '1dasilva', '1111-1111', 1)
+    RETURNING id
+    )
 INSERT INTO public.cliente (usuario_id,  fidelidade_pontos)
 SELECT id, 0.0
 FROM novo_usuario;
 --
 WITH novo_usuario AS (
-    INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
-        VALUES ('22222222222', '11-11-2014', 'usuario02@gmail.com', 'usuario02 de carvalho', '2decarvalho', '2222-2222', 2)
-        RETURNING id
-)
+INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
+VALUES ('22222222222', '11-11-2014', 'usuario02@gmail.com', 'usuario02 de carvalho', '2decarvalho', '2222-2222', 2)
+    RETURNING id
+    )
 INSERT INTO public.cliente (usuario_id,  fidelidade_pontos)
 SELECT id, 0.0
 FROM novo_usuario;
 --
 WITH novo_usuario AS (
-    INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
-        VALUES ('33333333333', '12-11-2014', 'usuario03@gmail.com', 'usuario03 pereira', '3pereira', '3333-3333', 3)
-        RETURNING id
-)
+INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
+VALUES ('33333333333', '12-11-2014', 'usuario03@gmail.com', 'usuario03 pereira', '3pereira', '3333-3333', 3)
+    RETURNING id
+    )
 INSERT INTO public.cliente (usuario_id,  fidelidade_pontos)
 SELECT id, 0.0
 FROM novo_usuario;
 
 -- Funcionários
 
--- INSERT INTO public.funcionario
--- (cpf, data_admissao, email, nome, senha, telefone, endereco_id, expediente, salario, cargo_id, farmacia_id)
--- VALUES('44444444444', '11-11-2014', 'usuario04@gmail.com', 'usuario04 ferreira', '4ferreira', '4444-4444', 4, 'manha', 1050.0, 1, 1);
---
--- INSERT INTO public.funcionario
--- (cpf, data_admissao, email, nome, senha, telefone, endereco_id, expediente, salario, cargo_id, farmacia_id)
--- VALUES('55555555555', '11-11-2014', 'usuario05@gmail.com', 'usuario05 oliveira', '5oliveira', '5555-5555', 5, 'tarde', 2010.0, 2, 2);
---
--- INSERT INTO public.funcionario
--- (cpf, data_admissao, email, nome, senha, telefone, endereco_id, expediente, salario, cargo_id, farmacia_id)
--- VALUES('66666666666', '11-11-2014', 'usuario06@gmail.com', 'usuario06 lopes', '6lopes', '6666-6666', 6, 'manha', 1450.0, 3, 3);
-
 WITH novo_usuario AS (
-    INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
-        VALUES ('44444444444', '11-11-2014', 'usuario04@gmail.com', 'usuario04 ferreira', '4ferreira', '4444-4444', 4)
-        RETURNING id
-)
+INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
+VALUES ('44444444444', '11-11-2014', 'usuario04@gmail.com', 'usuario04 ferreira', '4ferreira', '4444-4444', 4)
+    RETURNING id
+    )
 INSERT INTO public.funcionario (usuario_id,  expediente, salario, cargo_id, farmacia_id)
 SELECT id, 'manha', 1050.0, 1, 1
 FROM novo_usuario;
 --
 WITH novo_usuario AS (
-    INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
-        VALUES ('55555555555', '11-11-2014', 'usuario05@gmail.com', 'usuario05 oliveira', '5oliveira', '5555-5555', 5)
-        RETURNING id
-)
+INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
+VALUES ('55555555555', '11-11-2014', 'usuario05@gmail.com', 'usuario05 oliveira', '5oliveira', '5555-5555', 5)
+    RETURNING id
+    )
 INSERT INTO public.funcionario (usuario_id,  expediente, salario, cargo_id, farmacia_id)
 SELECT id, 'tarde', 2010.0, 2, 2
 FROM novo_usuario;
 --
 WITH novo_usuario AS (
-    INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
-        VALUES ('66666666666', '11-11-2014', 'usuario06@gmail.com', 'usuario06 lopes', '6lopes', '6666-6666', 6)
-        RETURNING id
-)
+INSERT INTO public.usuario (cpf, data_admissao, email, nome, senha, telefone, endereco_id)
+VALUES ('66666666666', '11-11-2014', 'usuario06@gmail.com', 'usuario06 lopes', '6lopes', '6666-6666', 6)
+    RETURNING id
+    )
 INSERT INTO public.funcionario (usuario_id,  expediente, salario, cargo_id, farmacia_id)
 SELECT id, 'manha', 1450.0, 3, 3
 FROM novo_usuario;
