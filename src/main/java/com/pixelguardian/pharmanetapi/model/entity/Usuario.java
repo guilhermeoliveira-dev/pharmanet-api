42f995008ecc473765d8ba17256ca79ccd0733d7
Guilherme de Almeida Oliveira <guilhermeovergame@gmail.com>
2025-07-31 22:37:51 -0300

    Tornar e-mail único

--- src/main/java/com/pixelguardian/pharmanetapi/model/entity/Usuario.java
+++ src/main/java/com/pixelguardian/pharmanetapi/model/entity/Usuario.java
@@ -20,6 +20,7 @@ public abstract class Usuario {
     private Long id;
 
     private String nome;
+    @Column(unique = true)
     private String email;
     private String senha;
     private String cpf;
