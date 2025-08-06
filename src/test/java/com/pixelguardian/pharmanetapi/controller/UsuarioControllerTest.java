f9bd71adf586385d8a757a47248f9fe7e55fc990
jair-ferreira-rodrigues-neto <jairferreiraneto1704mg@gmail.com>
2025-08-06 20:13:26 -0300

    Classe de teste da criptografia de senha

---
+++ src/test/java/com/pixelguardian/pharmanetapi/controller/UsuarioControllerTest.java
@@ -0,0 +1,19 @@
+package com.pixelguardian.pharmanetapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class UsuarioControllerTest {
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    public void post() {
+        String senhaCriptografada = passwordEncoder.encode("1dasilva");
+        System.out.println(senhaCriptografada);
+    }
+}
