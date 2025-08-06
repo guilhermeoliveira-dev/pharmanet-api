cb124d4a5bc5433fbe53fafe89c22837cc105173
Guilherme de Almeida Oliveira <guilhermeovergame@gmail.com>
2025-08-06 16:19:45 -0300

    Gerador de Códigos Alfanuméricos Aleatórios

--- src/main/java/com/pixelguardian/pharmanetapi/model/repository/PedidoCompraRepository.java
+++ src/main/java/com/pixelguardian/pharmanetapi/model/repository/PedidoCompraRepository.java
@@ -3,10 +3,15 @@ package com.pixelguardian.pharmanetapi.model.repository;
 import com.pixelguardian.pharmanetapi.model.entity.ItemPedido;
 import com.pixelguardian.pharmanetapi.model.entity.PedidoCompra;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PedidoCompraRepository extends JpaRepository<PedidoCompra, Long> {
+    @Query("select (count(p) > 0) from PedidoCompra p where p.codigo = ?1")
+    boolean existsByCodigo(String codigo);
+
+
 
 
 }
